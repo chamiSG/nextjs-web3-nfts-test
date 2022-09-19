113c07d95f96542a1ab31d89acff89f8773ee812
chamisg <snoblefuentes@gmail.com>
2022-09-19 10:49:18 -0700

    fix issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
-import NextDocument, { Html, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
+        <Head>
+          <title>NFTLaunchKit - Test Task</title>
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
